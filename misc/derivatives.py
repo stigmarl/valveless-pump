b35a41e395d05b0b68d4688e4122193b2bfabf67
Stig-Martin Liavåg <stigmartin94@gmail.com>
2017-12-07 12:02:27 +0100

    Added numba runtime compilation to derivatives.py

--- misc/derivatives.py
+++ misc/derivatives.py
@@ -1,7 +1,7 @@
 import numpy as np
+from numba import *
 
-
-
+@jit(float64[:,:](float64[:,:],float64[:,:],float64[:,:],float64))
 def dtt(f, f_1, f_2, dt):
     """
     Second order central difference differentiation in time.
@@ -25,7 +25,7 @@ def dtt(f, f_1, f_2, dt):
     """
     return (f[1:-1,1:-1] - 2*f_1[1:-1,1:-1] + f_2[1:-1,1:-1])/(dt**2)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dr_central(f, dr):
     """
     First order central difference differentiation.
@@ -46,46 +46,47 @@ def dr_central(f, dr):
     return (f[2:, 1:-1] - f[:-2,1:-1])/(2*dr)
 
 
+@jit(float64[:,:](float64[:,:],float64))
 def dzz(f, dz):
 
     return (f[1:-1, 2:] - 2*f[1:-1,1:-1] + f[1:-1, :-2])/(dz**2)
 
-
+@jit(float64[:,:](float64[:,:],float64[:,:],float64))
 def dt(f, f_1, dt):
 
     return (f[1:-1, 1:-1] - f_1[1:-1, 1:-1])/dt
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def drr(f, dr):
 
     return (f[2:,1:-1] - 2*f[1:-1,1:-1] + f[:-2, 1:-1])/(dr**2)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dz_central(f, dz):
 
     return (f[1:-1, 2:] - f[1:-1, :-2])/(2*dz)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dr_forward(f, dr):
 
     return (f[2:, 1:-1] - f[1:-1, 1:-1])/(dr)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dz_forward(f, dz):
 
     return (f[1:-1, 2:] - f[1:-1, 1:-1])/(dz)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dr_backward(f, dr):
 
     return (f[1:-1, 1:-1] - f[0:-2, 1:-1])/(dr)
 
-
+@jit(float64[:,:](float64[:,:],float64))
 def dz_backward(f, dz):
 
     return (f[1:-1, 1:-1] - f[1:-1, 0:-2])/(dz)
 
-
+@jit(float64[:,:](float64[:,:],float64,float64))
 def drz(f, dr, dz):
 
     return (f[2:, 2:] - f[:-2, 2:] - f[2:, :-2] + f[:-2, :-2])/(4*dr*dz)
